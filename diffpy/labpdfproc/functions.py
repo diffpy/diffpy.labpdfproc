5998b6f1156ebe5523be8969c42956817a927dfb
Simon Billinge <simon.billinge@gmail.com>
2024-02-26 23:30:27 -0500

    bit of cleaning now I got the package to install correctly

--- diffpy/labpdfproc/functions.py
+++ diffpy/labpdfproc/functions.py
@@ -1,10 +1,10 @@
-import numpy as np
+from diffpy.utils.scattering_objects.diffraction_objects import Diffraction_object
 
 wavelengths = {"Mo": 0.7107, "Ag": 0.59}
 def compute_cve(diffraction_data, mud, wavelength):
     # for a given mu and d and lambda, we will compute cve on a tth grid
     # something arbitrary for the moment
-    cve = diffpy.utils.DiffractionObject()
+    cve = Diffraction_object()
     cve_x = diffraction_data.on_tth[0]
     cve_y = cve_x * mud * wavelength
     cve.insert_scattering_quantity(cve_x, cve_y, "tth", metadata={ })
