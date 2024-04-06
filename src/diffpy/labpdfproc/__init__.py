be44a6b6d65648cde7203da5bdee67339ea09ce0
Simon Billinge <simon.billinge@gmail.com>
2024-04-06 09:06:26 -0400

    debugging ci

--- src/diffpy/labpdfproc/__init__.py
+++ src/diffpy/labpdfproc/__init__.py
@@ -1,4 +1,27 @@
-from ._version import get_versions
+#!/usr/bin/env python
+##############################################################################
+#
+# diffpy            by DANSE Diffraction group
+#                   Simon J. L. Billinge
+#                   (c) 2010 The Trustees of Columbia University
+#                   in the City of New York.  All rights reserved.
+#
+# File coded by:    Pavol Juhas
+#
+# See AUTHORS.txt for a list of people who contributed.
+# See LICENSE_DANSE.txt for license information.
+#
+##############################################################################
 
-__version__ = get_versions()["version"]
-del get_versions
+"""diffpy - tools for structure analysis by diffraction.
+
+Blank namespace package.
+"""
+
+
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)
+
+
+# End of file
