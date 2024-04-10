c1951834f7d7eaa73fe4a24f7f0709d9b9f3f2d8
yucongalicechen <yucong0821@g.ucla.edu>
2024-04-10 17:35:29 -0400

    initial commit

--- src/diffpy/labpdfproc/tests/test_functions.py
+++ src/diffpy/labpdfproc/tests/test_functions.py
@@ -18,16 +18,19 @@ def test_get_grid_points(inputs, expected):
         assert actual_point == pytest.approx(expected_point, rel=1e-4, abs=1e-6)
 
 
-params2 = [
-    ([1, 3, 1, 45], [0, 1.4142135, 1.4142135, 2, 2]),
-    ([1, 3, 1, 90], [0, 0, 2, 2, 2]),
-    ([1, 3, 1, 140], [0, 2, 0, 3.532, 1.2855]),
+params3 = [
+    ([1, 3, 1, 45], [1, 0.135335, 0.135335, 0.2431167, 0.2431167]),
+    ([1, 3, 1, 90], [1, 1, 0.135335, 0.135335, 0.135335]),
+    ([1, 3, 1, 120], [1, 1, 0.135335, 0.049787, 0.176921]),
 ]
 
 
-@pytest.mark.parametrize("inputs, expected", params2)
+@pytest.mark.parametrize("inputs, expected", params3)
 def test_set_distances_at_angle(inputs, expected):
-    expected_distances = expected
+    expected_muls = expected
     actual_gs = Gridded_circle(radius=inputs[0], n_points_on_diameter=inputs[1], mu=inputs[2])
     actual_gs.set_distances_at_angle(inputs[3])
-    assert actual_gs.distances == pytest.approx(expected_distances, rel=1e-4, abs=1e-6)
+    actual_gs.set_muls_at_angle(inputs[3])
+    actual_muls_sorted = sorted(actual_gs.muls)
+    expected_muls_sorted = sorted(expected_muls)
+    assert actual_muls_sorted == pytest.approx(expected_muls_sorted, rel=1e-4, abs=1e-6)
