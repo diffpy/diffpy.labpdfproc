53453ce46433d4dd52158a67969025addb4557d0
yucongalicechen <yucong0821@g.ucla.edu>
2024-04-10 20:43:59 -0400

    address comments: update inputs and remove set distances

--- src/diffpy/labpdfproc/tests/test_functions.py
+++ src/diffpy/labpdfproc/tests/test_functions.py
@@ -39,18 +39,16 @@ def test_set_distances_at_angle(inputs, expected):
 
 
 params3 = [
-    ([1, 3, 1, 45], [1, 0.2431167, 0.2431167, 0.135335, 0.135335]),
-    ([1, 3, 1, 90], [1, 1, 0.135335, 0.135335, 0.135335]),
-    ([1, 3, 1, 120], [1, 1, 0.135335, 0.049787, 0.176921]),
+    ([1], [1, 1, 0.135335, 0.049787, 0.176921]),
+    ([2], [1, 1, 0.018316, 0.002479, 0.031301]),
 ]
 
 
 @pytest.mark.parametrize("inputs, expected", params3)
 def test_set_muls_at_angle(inputs, expected):
     expected_muls = expected
-    actual_gs = Gridded_circle(radius=inputs[0], n_points_on_diameter=inputs[1], mu=inputs[2])
-    actual_gs.set_distances_at_angle(inputs[3])
-    actual_gs.set_muls_at_angle(inputs[3])
+    actual_gs = Gridded_circle(radius=1, n_points_on_diameter=3, mu=inputs[0])
+    actual_gs.set_muls_at_angle(120)
     actual_muls_sorted = sorted(actual_gs.muls)
     expected_muls_sorted = sorted(expected_muls)
     assert actual_muls_sorted == pytest.approx(expected_muls_sorted, rel=1e-4, abs=1e-6)
