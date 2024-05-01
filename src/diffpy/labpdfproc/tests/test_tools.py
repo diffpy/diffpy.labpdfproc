4208518dd1cb3a60089d61ab3830498e7e9ada22
yucongalicechen <yucong0821@g.ucla.edu>
2024-04-30 23:37:53 -0400

    included docstring and more tests

--- src/diffpy/labpdfproc/tests/test_tools.py
+++ src/diffpy/labpdfproc/tests/test_tools.py
@@ -15,5 +15,11 @@ params1 = [
 def test_set_output_directory(inputs, expected):
     expected_output_directory = expected[0]
     actual_args = argparse.Namespace(output_directory=inputs[0])
-    actual_output_directory = set_output_directory(actual_args)
-    assert actual_output_directory == expected_output_directory
+    actual_args.output_directory = set_output_directory(actual_args)
+    assert actual_args.output_directory == expected_output_directory
+    assert Path(actual_args.output_directory).exists()
+    assert Path(actual_args.output_directory).is_dir()
+
+    with pytest.raises(FileExistsError):
+        actual_args = argparse.Namespace(output_directory="test_tools.py")
+        actual_args.output_directory = set_output_directory(actual_args)
--- src/diffpy/labpdfproc/tools.py
+++ src/diffpy/labpdfproc/tools.py
@@ -2,6 +2,25 @@ from pathlib import Path
 
 
 def set_output_directory(args):
+    """
+    set the output directory based on the given input arguments
+
+    Parameters
+    ----------
+    args argparse.Namespace
+        input arguments provided by the user
+
+    Returns
+    -------
+    pathlib.PosixPath that contains the full path of the output directory
+
+    it is determined as follows:
+    If user provides an output directory, use it.
+    Otherwise, we set it to the current directory if nothing is provided.
+    We then create the directory if it does not exist.
+    If the provided directory is an existed file then we raise the FileExistsError.
+
+    """
     output_dir = Path(args.output_directory).resolve() if args.output_directory else Path.cwd()
     output_dir.mkdir(parents=True, exist_ok=True)
     return output_dir
