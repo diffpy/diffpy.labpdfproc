42ad6898d26a308249a1e0bb81f62733e449ec6a
yucongalicechen <yucong0821@g.ucla.edu>
2024-05-01 22:11:30 -0400

    save progress

--- src/diffpy/labpdfproc/tests/test_tools.py
+++ src/diffpy/labpdfproc/tests/test_tools.py
@@ -5,29 +5,6 @@ import pytest
 
 from diffpy.labpdfproc.tools import load_user_metadata, set_wavelength
 
-params5 = [
-    ([[]], []),
-    ([["toast=for breakfast"]], [["toast", "for breakfast"]]),
-    ([["mylist=[1,2,3.0]"]], [["mylist", "[1,2,3.0]"]]),
-    ([["weather=rainy", "day=tuesday"]], [["weather", "rainy"], ["day", "tuesday"]]),
-]
-
-
-@pytest.mark.parametrize("inputs, expected", params5)
-def test_load_user_metadata(inputs, expected):
-    actual_parser = ArgumentParser()
-    actual_parser.add_argument("-u", "--user-metadata", action="append", metavar="KEY=VALUE", nargs="+")
-    actual_args = actual_parser.parse_args([])
-    expected_parser = ArgumentParser()
-    expected_args = expected_parser.parse_args([])
-
-    setattr(actual_args, "user_metadata", inputs[0])
-    actual_args = load_user_metadata(actual_args)
-    for expected_pair in expected:
-        setattr(expected_args, expected_pair[0], expected_pair[1])
-    assert actual_args == expected_args
-
-
 params2 = [
     ([None, None], [0.71]),
     ([None, "Ag"], [0.59]),
@@ -56,3 +33,26 @@ def test_set_wavelength_bad(inputs):
     with pytest.raises(ValueError):
         actual_args = argparse.Namespace(wavelength=inputs[0], anode_type=inputs[1])
         actual_args.wavelength = set_wavelength(actual_args)
+
+
+params5 = [
+    ([[]], []),
+    ([["toast=for breakfast"]], [["toast", "for breakfast"]]),
+    ([["mylist=[1,2,3.0]"]], [["mylist", "[1,2,3.0]"]]),
+    ([["weather=rainy", "day=tuesday"]], [["weather", "rainy"], ["day", "tuesday"]]),
+]
+
+
+@pytest.mark.parametrize("inputs, expected", params5)
+def test_load_user_metadata(inputs, expected):
+    actual_parser = ArgumentParser()
+    actual_parser.add_argument("-u", "--user-metadata", action="append", metavar="KEY=VALUE", nargs="+")
+    actual_args = actual_parser.parse_args([])
+    expected_parser = ArgumentParser()
+    expected_args = expected_parser.parse_args([])
+
+    setattr(actual_args, "user_metadata", inputs[0])
+    actual_args = load_user_metadata(actual_args)
+    for expected_pair in expected:
+        setattr(expected_args, expected_pair[0], expected_pair[1])
+    assert actual_args == expected_args
--- src/diffpy/labpdfproc/tools.py
+++ src/diffpy/labpdfproc/tools.py
@@ -2,39 +2,6 @@ WAVELENGTHS = {"Mo": 0.71, "Ag": 0.59, "Cu": 1.54}
 known_sources = [key for key in WAVELENGTHS.keys()]
 
 
-def load_key_value_pair(s):
-    items = s.split("=")
-    key = items[0].strip()
-    if len(items) > 1:
-        value = "=".join(items[1:])
-    return (key, value)
-
-
-def load_user_metadata(args):
-    if args.user_metadata:
-        for item in args.user_metadata:
-            if "=" not in item:
-                raise ValueError("please provide key-value pairs in the format key=value.")
-            key, value = load_key_value_pair(item)
-            setattr(args, key, value)
-    delattr(args, "user_metadata")
-    return args
-
-
-# Notes, can ignore
-# ideal inputs: "key1=value1" "key2=value2"
-# (different pairs are separated by white spaces, key and value are separated by =)
-
-# question 1: white spaces in key, value, both?
-# if value contains whitespace, you can specify key="value value" or "key=value value"
-# if key contains whitespace, for example, you have "key key=value",
-# then you can access like getattr(args, 'key key'), but we dont recommend this
-
-# question 2: more than one =?
-# if i have key is hello, value is hello=world, then i can have hello=hello=world to process okay
-# if i have = in key then it's still processing as value => would this be an issue?
-
-
 def set_wavelength(args):
     """
     Set the wavelength based on the given input arguments
@@ -67,3 +34,36 @@ def set_wavelength(args):
         return WAVELENGTHS[args.anode_type]
     else:
         return WAVELENGTHS["Mo"]
+
+
+def _load_key_value_pair(s):
+    items = s.split("=")
+    key = items[0].strip()
+    if len(items) > 1:
+        value = "=".join(items[1:])
+    return (key, value)
+
+
+def load_user_metadata(args):
+    if args.user_metadata:
+        for item in args.user_metadata:
+            if "=" not in item:
+                raise ValueError("please provide key-value pairs in the format key=value.")
+            key, value = _load_key_value_pair(item)
+            setattr(args, key, value)
+    delattr(args, "user_metadata")
+    return args
+
+
+# Notes, can ignore
+# ideal inputs: "key1=value1" "key2=value2"
+# (different pairs are separated by white spaces, key and value are separated by =)
+
+# question 1: white spaces in key, value, both?
+# if value contains whitespace, you can specify key="value value" or "key=value value"
+# if key contains whitespace, for example, you have "key key=value",
+# then you can access like getattr(args, 'key key'), but we dont recommend this
+
+# question 2: more than one =?
+# if i have key is hello, value is hello=world, then i can have hello=hello=world to process okay
+# if i have = in key then it's still processing as value => would this be an issue?
