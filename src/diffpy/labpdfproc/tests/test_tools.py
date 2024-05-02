8e9c8ecc0ba7a2f787ed1d08f7750c82f4c1b8ef
Simon Billinge <simon.billinge@gmail.com>
2024-05-01 21:15:15 -0400

    tweaking tests

--- src/diffpy/labpdfproc/tests/test_tools.py
+++ src/diffpy/labpdfproc/tests/test_tools.py
@@ -1,24 +1,26 @@
 import argparse
+import os
 from pathlib import Path
 
 import pytest
 
-from diffpy.labpdfproc.tools import set_output_directory, set_wavelength, known_sources
+from diffpy.labpdfproc.tools import known_sources, set_output_directory, set_wavelength
 
 params1 = [
-    ([None], [Path.cwd().resolve()]),
-    (["."], [Path.cwd().resolve()]),
-    (["new_dir"], [Path.cwd().resolve() / "new_dir"]),
-    (["existing_dir"], [Path.cwd().resolve() / "existing_dir"]),
+    ([None], ["."]),
+    (["."], ["."]),
+    (["new_dir"], ["new_dir"]),
+    (["existing_dir"], ["existing_dir"]),
 ]
 
 
 @pytest.mark.parametrize("inputs, expected", params1)
-def test_set_output_directory(inputs, expected):
-    existing_dir = Path().cwd().resolve() / "existing_dir"
+def test_set_output_directory(inputs, expected, tmpdir):
+    existing_dir = Path(tmpdir) / "existing_dir"
     existing_dir.mkdir(parents=True, exist_ok=True)
+    os.chdir(tmpdir)
 
-    expected_output_directory = expected[0]
+    expected_output_directory = Path(tmpdir).resolve() / expected[0]
     actual_args = argparse.Namespace(output_directory=inputs[0])
     actual_args.output_directory = set_output_directory(actual_args)
     assert actual_args.output_directory == expected_output_directory
@@ -26,9 +28,10 @@ def test_set_output_directory(inputs, expected):
     assert Path(actual_args.output_directory).is_dir()
 
 
-def test_set_output_directory_bad():
-    existing_file = Path().cwd().resolve() / "existing_file.py"
+def test_set_output_directory_bad(tmpdir):
+    existing_file = Path(tmpdir) / "existing_file.py"
     existing_file.touch()
+    os.chdir(tmpdir)
 
     actual_args = argparse.Namespace(output_directory="existing_file.py")
     with pytest.raises(FileExistsError):
